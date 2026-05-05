f97c103a0cbe85d0a4d67581da5ba2c2d440c5aa
agusmoyano-h <137466326+agusmoyano-h@users.noreply.github.com>
2026-05-05 10:53:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function test() {
+  alert("App funcionando correctamente 🚀");
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((reg) => {
+        console.log("SW registrado:", reg.scope);
+      })
+      .catch((err) => {
+        console.error("Error SW:", err);
+      });
+  });
+}
---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "gym-pwa-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
